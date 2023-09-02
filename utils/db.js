05db556036f62557388b6ee060c3ccd2d220a28c
koredeycode <koredey4u@gmail.com>
2023-09-02 22:36:44 +0100

    test

--- utils/db.js
+++ utils/db.js
@@ -1,99 +1,64 @@
-import * as dotenv from 'dotenv';
-import sha1 from 'sha1';
-import { MongoClient, ObjectId } from 'mongodb';
+import mongodb from 'mongodb';
+// eslint-disable-next-line no-unused-vars
+import Collection from 'mongodb/lib/collection';
+import envLoader from './env_loader';
 
-dotenv.config({
-  path: process.env.npm_lifecycle_event.includes('test') ? '.env.test' : '.env',
-});
-
+/**
+ * Represents a MongoDB client.
+ */
 class DBClient {
+  /**
+   * Creates a new DBClient instance.
+   */
   constructor() {
+    envLoader();
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
-    const clusterUser = process.env.CLUSTER_USER;
-    const clusterPwd = process.env.CLUSTER_PWD;
-    let url;
+    const dbURL = `mongodb://${host}:${port}/${database}`;
 
-    if (clusterUser && clusterPwd) {
-      url = `mongodb+srv://${clusterUser}:${clusterPwd}@cluster0.l5awlge.mongodb.net/${database}`;
-    } else {
-      url = `mongodb://${host}:${port}/${database}`;
-    }
-
-    this.client = new MongoClient(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    this.client = new mongodb.MongoClient(dbURL, { useUnifiedTopology: true });
     this.client.connect();
   }
 
+  /**
+   * Checks if this client's connection to the MongoDB server is active.
+   * @returns {boolean}
+   */
   isAlive() {
     return this.client.isConnected();
   }
 
+  /**
+   * Retrieves the number of users in the database.
+   * @returns {Promise<Number>}
+   */
   async nbUsers() {
     return this.client.db().collection('users').countDocuments();
   }
 
+  /**
+   * Retrieves the number of files in the database.
+   * @returns {Promise<Number>}
+   */
   async nbFiles() {
     return this.client.db().collection('files').countDocuments();
   }
 
-  async createUser(email, password) {
-    const user = await this.client.db().collection('users').findOne({ email });
-    if (user) throw Error('Already exists');
-    const hashedPwd = sha1(password);
-    return this.client
-      .db()
-      .collection('users')
-      .insertOne({ email, password: hashedPwd });
+  /**
+   * Retrieves a reference to the `users` collection.
+   * @returns {Promise<Collection>}
+   */
+  async usersCollection() {
+    return this.client.db().collection('users');
   }
 
-  async findUser(_query) {
-    const query = _query;
-    if (query._id) query._id = ObjectId(query._id);
-    const user = await this.client.db().collection('users').findOne(query);
-    return user;
-  }
-
-  async createFolder(folderInfo) {
-    return this.client.db().collection('files').insertOne(folderInfo);
-  }
-
-  async findFolder(_query) {
-    const query = _query;
-    if (query._id) query._id = ObjectId(query._id);
-    const folder = await this.client.db().collection('files').findOne(query);
-    return folder;
-  }
-
-  async findFile(_query) {
-    const query = _query;
-    if (query._id) query._id = ObjectId(query._id);
-    const file = await this.client
-      .db()
-      .collection('files')
-      .findOne(query, { _id: 0, id: '$_id' });
-    return file;
-  }
-
-  async findFiles(pipeline) {
-    return this.client.db().collection('files').aggregate(pipeline).toArray();
-  }
-
-  async createFile(fileInfo) {
-    return this.client.db().collection('files').insertOne(fileInfo);
-  }
-
-  async updateFile(id, update) {
-    const newUpdate = { $set: update };
-    const query = { _id: ObjectId(id) };
-    const file = await this.client
-      .db()
-      .collection('files')
-      .updateOne(query, newUpdate);
-    return file;
+  /**
+   * Retrieves a reference to the `files` collection.
+   * @returns {Promise<Collection>}
+   */
+  async filesCollection() {
+    return this.client.db().collection('files');
   }
 }
 
