eb4325aeaf89716d54f722c83e1deb92d3d0473f
koredeycode <koredey4u@gmail.com>
2023-09-03 21:23:29 +0100

    fixed error msg to json

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,14 +2,14 @@ import { getUserFromAuthorization, getUserFromToken } from '../utils/auth';
 
 export async function basicAuth(req, res, next) {
   const user = await getUserFromAuthorization(req);
-  if (!user) return res.status(401).send({ error: 'Unauthorized' });
+  if (!user) return res.status(401).json({ error: 'Unauthorized' });
   req.user = { user };
   next();
 }
 
 export async function tokenAuth(req, res, next) {
   const user = await getUserFromToken(req);
-  if (!user) return res.status(401).send({ error: 'Unauthorized' });
+  if (!user) return res.status(401).json({ error: 'Unauthorized' });
   req.user = { user };
   next();
 }
